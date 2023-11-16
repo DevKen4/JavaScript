2faf7465eac3cc476e8a6fdc2a64fd0a7c0a2456
Ken Johnson <othuko47@gmail.com>
2023-11-16 11:55:16 -0800

    first javascript challenge

---
+++ JS/js/main.js
@@ -0,0 +1,7 @@
+//My first code challenge
+//Write a code a code that will return a random letter from your name
+
+console.log("Kenny".charAt(Math.floor(Math.random() * 5)));
+console.log("Kenny".charAt(Math.floor(Math.random() * 5)));
+console.log("Kenny".charAt(Math.floor(Math.random() * 5)));
+console.log("Kenny".charAt(Math.floor(Math.random() * 5)));
